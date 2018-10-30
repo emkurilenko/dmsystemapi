38f465d9ea907960b0c3dcd0facf9a918ef56282
emkuilenko <kurilenko.e.m@outlook.com>
2018-10-30 23:35:01 +0300

    add controller create new file

--- src/main/java/com/kurilenko/dmsystemapi/config/JpaConfiguration.java
+++ src/main/java/com/kurilenko/dmsystemapi/config/JpaConfiguration.java
@@ -58,6 +58,7 @@ public class JpaConfiguration {
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+
         return properties;
     }
 
